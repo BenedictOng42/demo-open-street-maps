17113a2c2009da4f8b18d17805af5f91d0e88942
Benedict Ong <benedictong42@gmail.com>
2020-07-25 01:06:59 +1000

    feat: beginngings

--- src/pages/MapsPage/index.tsx
+++ src/pages/MapsPage/index.tsx
@@ -1,9 +1,23 @@
 import React, { FC } from "react";
 import { useParams } from "react-router";
+import MapboxGLMap from "../../components/MapboxGLMap";
+import EventCard from "../../components/EventCard";
+import { useDocumentData } from "../../hooks/firebase";
+import { Event } from "../../types";
 
 const MapsPage: FC = () => {
-  const { id } = useParams<{ id?: string }>();
-  return <div>Map {id}</div>;
+  const { id: eventId } = useParams<{ id?: string }>();
+  const [event, error] = useDocumentData<Event>(`events/${eventId}`);
+
+  return (
+    <>
+      <EventCard />
+      <MapboxGLMap
+        // longitude={event?.location.longitude}
+        latitude={event?.location.latitude}
+      />
+    </>
+  );
 };
 
 export default MapsPage;
